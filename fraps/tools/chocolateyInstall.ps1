5d738699b47813c040e6f65b3c3d046d0e38d2db
Jean-Philippe Steinmetz <caskater47@gmail.com>
2015-01-02 15:28:05 -0800

    Adding fraps package for chocolatey

---
+++ fraps/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'fraps'
+$installerType = 'EXE'
+$url = 'http://www.fraps.com/free/setup.exe'
+$silentArgs = '/S /D=C:\Fraps'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
