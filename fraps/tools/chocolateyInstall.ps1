7c3f50550d0b16163203f5ff30bc61337a338663
Jean-Philippe Steinmetz <caskater47@gmail.com>
2015-01-03 16:42:51 -0800

    Updating fraps package according to recommendations Resizing fraps icon to 128x128

--- fraps/tools/chocolateyInstall.ps1
+++ fraps/tools/chocolateyInstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = 'fraps'
 $installerType = 'EXE'
 $url = 'http://www.fraps.com/free/setup.exe'
-$silentArgs = '/S /D=C:\Fraps'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
