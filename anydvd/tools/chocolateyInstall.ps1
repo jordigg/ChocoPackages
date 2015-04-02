5825e99ab536a4b5f11783c9c41617b17eb54931
Jean-Philippe Steinmetz <caskater47@gmail.com>
2015-04-01 18:15:40 -0700

    Updating AnyDVD package to 7.5.9.0

--- anydvd/tools/chocolateyInstall.ps1
+++ anydvd/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'anydvd'
 $installerType = 'EXE'
-$url = 'http://static.slysoft.com/SetupAnyDVD7570.exe'
+$url = 'http://static.slysoft.com/SetupAnyDVD7590.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+ 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
