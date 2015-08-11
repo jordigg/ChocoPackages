cee0ae971fbc42e7c44de2bfaed49f74c2afe623
Jean-Philippe Steinmetz <caskater47@gmail.com>
2015-08-10 17:15:50 -0700

    Updating TeamSpeak to 3.0.17

--- teamspeak/tools/chocolateyInstall.ps1
+++ teamspeak/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'teamspeak'
 $installerType = 'EXE'
-$url = 'http://dl.4players.de/ts/releases/3.0.16/TeamSpeak3-Client-win32-3.0.16.exe'
-$url64 = 'http://dl.4players.de/ts/releases/3.0.16/TeamSpeak3-Client-win64-3.0.16.exe'
+$url = 'http://dl.4players.de/ts/releases/3.0.17/TeamSpeak3-Client-win32-3.0.17.exe'
+$url64 = 'http://dl.4players.de/ts/releases/3.0.17/TeamSpeak3-Client-win64-3.0.17.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
