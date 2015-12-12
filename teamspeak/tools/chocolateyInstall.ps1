97e791c38e27beb001360d6f8c45be506510c7a3
Jean-Philippe Steinmetz <caskater47@gmail.com>
2015-12-12 13:49:56 -0800

    [ChocoPackages] Updating Teamspeak to 3.0.18.2

--- teamspeak/tools/chocolateyInstall.ps1
+++ teamspeak/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'teamspeak'
 $installerType = 'EXE'
-$url = 'http://dl.4players.de/ts/releases/3.0.17/TeamSpeak3-Client-win32-3.0.17.exe'
-$url64 = 'http://dl.4players.de/ts/releases/3.0.17/TeamSpeak3-Client-win64-3.0.17.exe'
+$url = 'http://dl.4players.de/ts/releases/3.0.18.2/TeamSpeak3-Client-win32-3.0.18.2.exe'
+$url64 = 'http://dl.4players.de/ts/releases/3.0.18.2/TeamSpeak3-Client-win64-3.0.18.2.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
