1e33de5de5862d89b03cc231df90e7c0acf892a8
Jordi Garcia <jordi@improbable.io>
2016-11-02 16:35:55 +0000

    Updated package with best practices

--- p4/tools/chocolateyInstall.ps1
+++ p4/tools/chocolateyInstall.ps1
@@ -1,8 +1,26 @@
-﻿$packageName = 'p4'
-$installerType = 'EXE'
-$url = 'http://cdist2.perforce.com/perforce/r15.2/bin.ntx86/helix-p4-x86.exe'
-$url64 = 'http://cdist2.perforce.com/perforce/r15.2/bin.ntx64/helix-p4-x64.exe'
-$silentArgs = '/s /v"/qn"'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$packageName = 'p4'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://filehost.perforce.com/perforce/r16.1/bin.ntx86/helix-p4-x86.exe'
+$url64       = 'http://filehost.perforce.com/perforce/r16.1/bin.ntx64/helix-p4-x64.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  url64bit       = $url64
+
+  softwareName   = 'Helix P4 Command-Line Client*'
+
+  checksum       = 'B66FE09D472E1532559E56602814F6423B6F65ABEE0B30AA3E314663DE870B80'
+  checksumType   = 'sha256'
+  checksum64     = '0CCD52CE74874503ED537C6D44050C551BC16392F09B782EE88EDF17F0589EE5'
+  checksumType64 = 'sha256'
+
+  silentArgs     = '/s /v"/qn"'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
